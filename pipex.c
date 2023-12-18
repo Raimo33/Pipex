f807a709fece5d59073f2494eccd6c88dc2c244d
Claudio Raimondi <craimond@c1r3p4.42firenze.it>
2023-12-18 17:35:15 +0100

    bonus prototype

--- pipex.c
+++ pipex.c
@@ -6,49 +6,60 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 15:17:44 by craimond          #+#    #+#             */
-/*   Updated: 2023/12/17 19:06:21 by craimond         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:34:01 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void			child(int fds[], char **argv, char *path);
-static void			parent(int fds[], char **argv, char *path);
+static void			child(int fds[], int prev_out, char **argv, char *path);
+static void			last_pipe(int fds[], char **argv, char *path);
 
 struct s_buffers	buffers;
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		id;
-	char	*path;
+	pid_t			id;
+	char			*path;
+	int 			fds[4]; // infile, outfile, pipe read, pipe write
+	int				prev_out;
+	int				tmp;
 
-	int fds[4]; // infile, outfile, pipe read, pipe write
-	if (argc < 5 || pipe(fds + 2) == -1)
+	if (argc < 5)
 		error(1);
-	while (ft_strncmp(*envp, "PATH", 4))
+	while (*envp && ft_strncmp(*envp, "PATH", 4))
 		envp++;
 	path = *envp + 5;
-	id = fork();
-	if (id == -1)
+	prev_out = open(++argv, O_RDONLY);
+	if (prev_out == -1)
 		error(2);
-	if (id == 0)
-		child(fds, argv, path);
-	else
-		parent(fds, argv, path);
+	while (argc-- > 4)
+	{
+		id = fork();
+		if (id == -1 || pipe(fds + 2) == -1)
+			error(3);
+		if (id == 0)
+			child(fds, prev_out, ++argv, path);
+		if (close(fds[3]) == -1 || close(prev_out) == -1)
+			error(4);
+		prev_out = fds[2];
+	}
+	while (wait(NULL) > 0)
+		;
+	if (errno != ECHILD)
+		error(5);
+	last_pipe(fds, argv, path);
 	error(0);
 }
 
-static void	child(int fds[], char **argv, char *path)
+static void	child(int fds[], int prev_out, char **argv, char *path)
 {
 	char	**cmd_args;
 	char	*cmd_path;
 
-	if (close(fds[2]) == -1 || dup2(fds[3], STDOUT_FILENO) == -1 || close(fds[3]) == -1)
+	if (close(fds[2]) == -1 || dup2(prev_out, STDIN_FILENO) == -1 || dup2(fds[3], STDOUT_FILENO) == -1 || close(fds[3]) == -1 || close(fds[0]) == -1)
 		error(3);
-	fds[0] = open(argv[1], O_RDONLY);
-	if (fds[0] == -1 || dup2(fds[0], STDIN_FILENO) == -1 || close(fds[0]) == -1)
-		error(4);
-	cmd_args = ft_split(argv[2], ' ');
+	cmd_args = ft_split(*argv, ' ');
 	buffers.cmd_args_child = cmd_args;
 	cmd_path = find_cmd(path, cmd_args[0]);
 	buffers.cmd_path_child = cmd_path;
@@ -56,18 +67,18 @@ static void	child(int fds[], char **argv, char *path)
 		error(5);
 }
 
-static void	parent(int fds[], char **argv, char *path)
+static void	last_pipe(int fds[], char **argv, char *path)
 {
 	char	**cmd_args;
 	char	*cmd_path;
 
-	if (close(fds[3]) || waitpid(0, NULL, 0) == -1 || dup2(fds[2], STDIN_FILENO) == -1 || close(fds[2]) == -1)
+	if (close(fds[3]) || dup2(fds[2], STDIN_FILENO) == -1 || close(fds[2]) == -1)
 		error(6);
-	cmd_args = ft_split(argv[3], ' ');
+	cmd_args = ft_split(*argv, ' ');
 	buffers.cmd_args_parent = cmd_args;
 	cmd_path = find_cmd(path, cmd_args[0]);
 	buffers.cmd_path_parent = cmd_path;
-	fds[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fds[1] = open(*argv, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fds[1] == -1 || dup2(fds[1], STDOUT_FILENO) == -1 || close(fds[1]) == -1 || execve(cmd_path, cmd_args, environ) == -1)
 		error(7);
 }
