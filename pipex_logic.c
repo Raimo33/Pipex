b94c26a84f440f787081cbe8468fed8c6b73cd18
Claudio Raimondi <craimond@c2r3p10.42firenze.it>
2023-12-24 14:41:18 +0100

    organized bonus files

--- pipex_logic.c
+++ pipex_logic.c
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 14:25:21 by craimond          #+#    #+#             */
-/*   Updated: 2023/12/24 14:33:44 by craimond         ###   ########.fr       */
+/*   Updated: 2023/12/24 14:40:24 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,16 @@ void	handle_command(int fds[], char **argv, char *path, char **envp)
 	pid_t	id;
 	
 	if (pipe(fds + 2) == -1)
-		quit(7, NULL, 0);
+		quit(3, NULL, 0);
 	id = fork();
 	if (id == -1)
-		quit(8, NULL, 0);
+		quit(4, NULL, 0);
 	if (id == 0)
 		handle_pipe(fds, argv, path, envp);
 	else
 	{
 		if (close(fds[0]) == -1 || close(fds[3]) == -1)
-			quit(9, NULL, 0);
+			quit(5, NULL, 0);
 		fds[0] = fds[2];
 	}
 }
@@ -56,11 +56,11 @@ void	handle_pipe(int fds[], char **argv, char *path, char **envp)
 void	last_pipe(int fds[], char **argv, char *path, char **envp)
 {
 	if (dup2(fds[0], STDIN_FILENO) == -1 || dup2(fds[1], STDOUT_FILENO) == -1 || close(fds[1]) == -1 || close(fds[0]) == -1)
-		quit(8, "", 0);
+		quit(6, "", 0);
 	buffers.cmd_args = ft_split(*argv, ' ');
 	buffers.cmd_path = find_cmd(path, buffers.cmd_args[0]);
 	if (execve(buffers.cmd_path, buffers.cmd_args, envp) == -1)
-		quit(3, "", 0);
+		quit(7, "", 0);
 }
 
 void	wait_child(void)
@@ -68,7 +68,7 @@ void	wait_child(void)
 	int		status;
 
 	if (wait(&status) == -1)
-		quit(3, NULL, 0);
+		quit(8, NULL, 0);
 	if (WIFSIGNALED(status))
 		quit(WTERMSIG(status) + 128, "process interrupted by a signal", 32);
 	else
@@ -77,7 +77,7 @@ void	wait_child(void)
 		if (status > 42)
 		{
 			errno = status - 42;
-			quit(4, NULL, 0);
+			quit(9, NULL, 0);
 		}
 	}
 }
--- pipex_utils.c
+++ pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 13:57:18 by craimond          #+#    #+#             */
-/*   Updated: 2023/12/24 14:34:01 by craimond         ###   ########.fr       */
+/*   Updated: 2023/12/24 14:40:40 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char *get_path(char **envp)
 	while (envp && *envp)
 		if (ft_strncmp(*envp++, "PATH=", 5) == 0)
 			return (*(envp - 1) + 5);
-	quit(11, "PATH not found", 15);
+	quit(10, "PATH not found", 15);
 	return (NULL);
 }
 
@@ -68,7 +68,7 @@ char	**ft_split(char *s, char c)
 			n_words++;
 	str_array = malloc(sizeof(char *) * (n_words + 1));
 	if (!str_array)
-		quit(10, "failed to allocate memory", 26);
+		quit(11, "failed to allocate memory", 26);
 	buffers.str_array = str_array;
 	str_array[n_words] = NULL;
 	return (fill_matrix(n_words, s, c, str_array));
@@ -89,7 +89,7 @@ static char	**fill_matrix(unsigned int n_words, char *s, char c, char **str_arra
 			len++;
 		str_array[g] = malloc(sizeof(char) * (len + 1));
 		if (!str_array[g])
-			quit(11, "failed to allocate memory", 26);
+			quit(12, "failed to allocate memory", 26);
 		ft_strncpy(str_array[g], s, len);
 		str_array[g][len] = '\0';
 		s += len;
